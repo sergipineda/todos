c0fc3455dcdc51fcfb83e20c605552f834aa47a7
Sergi <spineda1994@gmail.com>
2015-11-06 17:28:35 +0100

    Initial commit

---
+++ app/src/androidTest/java/com/iesebre/dam2/spineda/todos2/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.iesebre.dam2.spineda.todos2;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
